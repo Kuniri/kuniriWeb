c95c0fa6441fc07c8d98674767ff681cf3e8708e
laisbarreto <lais_barreto92@hotmail.com>
2015-05-19 19:53:56 -0300

    Implementation of the faild step of BDD cycle - issue #1

--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -1,7 +1,26 @@
-When(/^I click in "(.*?)"$/) do |arg1|
-	
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
 end
 
-Then(/^I should see "(.*)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^(?:|I )follow "([^"]*)"$/ do |link|
+  click_link(link)
 end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
+
+And /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
+
