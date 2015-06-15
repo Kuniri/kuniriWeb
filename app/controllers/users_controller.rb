8e8569b61726494be688198432ed0ca6d98b8ac8
cotrim <cotrim149@gmail.com>
2015-06-15 00:13:39 -0300

    Saving new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to '/kuniri'
+		else
+			redirect_to '/sign_up'
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:first_name,:last_name, :email, :password_digest)
+		end
+
 end
