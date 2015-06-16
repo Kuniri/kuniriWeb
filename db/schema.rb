72bd1ae439fe91b1986658d7e0ea5f5a90f310a6
cotrim <cotrim149@gmail.com>
2015-06-16 17:59:45 -0300

    Add analyse field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615023923) do
+ActiveRecord::Schema.define(version: 20150616205346) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
