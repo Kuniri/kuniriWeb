9c8454aead041c3d7aff231f8d5c0c9f7f06fd3e
cotrim <cotrim149@gmail.com>
2015-06-16 15:01:14 -0300

    fix some part of bug of login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,25 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:session][:email])
 
+    if @user.nil?
+    	flash.now[:error] = 'Invalid User. user do not registered'
+    	#redirect_to 'login' and return
+    end    	
+
 		if @user.password.nil?
     	flash.now[:error] = 'Invalid Password. Password is nil'			
-			redirect_to 'login'
+			#redirect_to 'login' and return
 		end
 
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/kuniri'
+		  redirect_to '/kuniri' and return
     else
     	flash.now[:error] = 'Invalid email/password combination'
-      redirect_to 'login'
-    end  
+      redirect_to 'login' and return
+    end
+
+
   end
   
 end
--- db/migrate/20150615023923_create_users.rb
+++ db/migrate/20150615023923_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :last_name
       t.string :email
       t.string :password_digest
-#      t.string :password_confirmation
 
       t.timestamps null: false
     end
