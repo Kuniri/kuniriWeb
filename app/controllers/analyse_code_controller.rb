b0879bccdf15d59a81c4f95a74ac0e817f3f4dea
cotrim <cotrim149@gmail.com>
2015-06-18 17:36:19 -0300

    Project are linked with user id

--- app/controllers/analyse_code_controller.rb
+++ app/controllers/analyse_code_controller.rb
@@ -7,9 +7,11 @@ class AnalyseCodeController < ApplicationController
 
 	def create
 		@project = Project.new(project_param)
+		@project.name = "Diagrama 1"
+		@project.user_id = current_user.id
 
 		if @project.save
-			flash[:notice] = "project analysed with success. user id: #{current_user.id}"
+			flash[:notice] = "project analysed with success. user id: #{@project.user_id}"
 			redirect_to '/kuniri' and return
 		else
 			redirect_to '/analyse_code' and return
@@ -22,7 +24,7 @@ class AnalyseCodeController < ApplicationController
 	private
 
 		def project_param
-			params.require(:project).permit(:link,current_user.id)
+			params.require(:project).permit(:name,:link,:user_id)
 		end
 
 end
