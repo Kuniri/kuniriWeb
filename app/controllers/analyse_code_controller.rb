7b16568cbc2d6bf7ced7a314012c63abed3b3981
cotrim <cotrim149@gmail.com>
2015-06-22 20:52:39 -0300

    modified .gitignore with temporary folder of projects, add flash notice in application main view and refactored part of analysed code controller

--- app/controllers/analyse_code_controller.rb
+++ app/controllers/analyse_code_controller.rb
@@ -7,26 +7,23 @@ class AnalyseCodeController < ApplicationController
 
 	def create
 		@project = Project.new(project_param)
-		@project.name = extract_name_project(@project.link)
+		@project.name = extract_name_project
 		@project.user_id = current_user.id
 
-		analyse_code(@project.link)
+		analyse_code
 
 		if @project.save
-			flash[:notice] = "project analysed with success. user id: #{@project.user_id}"
+			#flash[:notice] = "project submited with success."
 			redirect_to '/kuniri' and return
 		else
 			redirect_to '/analyse_code' and return
 		end
 	end
 
-	def analyse_code(link)
-		
-	end
-
-	def extract_name_project(link)
-		name = /https:\/\/github.com\/(\w*)\/(\w*)/.match(link)
-		name[2]
+	def analyse_code
+		create_local_repo
+		clone_repo_from_link
+		run_kuniry
 	end
 
 	private
@@ -35,4 +32,21 @@ class AnalyseCodeController < ApplicationController
 			params.require(:project).permit(:name,:link,:user_id)
 		end
 
+		def extract_name_project
+			name = /https:\/\/github.com\/(\w*)\/(\w*)/.match(@project.link)
+			name[2]
+		end
+
+		def create_local_repo
+			system("mkdir projects")
+		end
+
+		def clone_repo_from_link
+			cmd_clone = "git clone #{@project.link} projects/#{@project.name}"
+			system(cmd_clone)
+		end
+
+		def run_kuniry
+			flash[:notice] = "Project analysed with success!"
+		end
 end
