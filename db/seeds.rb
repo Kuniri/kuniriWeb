3c491e673190d834e6fd9bbbf28425e65c506571
laisbarreto <lais_barreto92@hotmail.com>
2015-06-22 19:16:17 -0300

    Addin admin account and admin permission for deleting users on index by delete link, invisible when not admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(first_name:  "Lais",
+             last_name: "Araujo",
+             email: "admin@admin",
+             password:              "admin",
+             password_confirmation: "admin",
+             admin: true)
+
+99.times do |n|
+  name  = "Jonas"
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
