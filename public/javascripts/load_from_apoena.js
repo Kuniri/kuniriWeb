b0da86b1b2b3282dd02af8e936de52bd7bbd7904
cotrim <cotrim149@gmail.com>
2015-06-25 15:31:16 -0300

    add integration with kuniri. Now kuniriWeb run analysis and generate xml.

--- public/javascripts/load_from_apoena.js
+++ public/javascripts/load_from_apoena.js
@@ -1,5 +1,8 @@
 
-
+id = document.getElementById("class_diagram").getAttribute("userID")
+project_name = document.getElementById("class_diagram").getAttribute("projectName")
 apo.reloadCanvas("class_diagram")
 import_class("class_data.xml")
 apo.draw()
+
+//public/id_user/projectName/class_data.xml
